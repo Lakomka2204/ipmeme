b463f2b9fe32deebbe92150bac8f1e8a393c5ef0
Lakomka <denis.kozachok22@gmail.com>
2021-11-23 19:22:50 +0200

    initial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.</title>
+    <link rel="stylesheet" href="s.css">
+    <script type="text/javascript" src="xd.js"></script>
+</head>
+<body style="background-color: black;">
+    <h1 hidden style="color: white;" id="txt"><?php echo $_SERVER['REMOTE_ADDR'];?></h1>
+    <video hidden loop nodownload preload="auto" poster="a.jpg" id="vp">
+        <source src="o.mp4" type="video/mp4">
+    </video>
+    <button id="b" onclick="ReleaseTheMonster();"><h2>Click to see more hentai</h2></button>
+</body>
+</html>
---
+++ xd.js
@@ -0,0 +1,20 @@
+function ReleaseTheMonster()
+{
+    var v = document.getElementById('vp');
+    v.hidden = false;
+    var b = document.getElementById('b');
+    b.hidden = true;
+    var txt = document.getElementById('txt');
+    txt.hidden = false;
+    b.remove()
+    var video = document.getElementById("vp");
+    video.muted = false;
+    video.play()
+}
+function delz()
+{
+    var c = document.getElementsByTagName("div")[0]
+    console.log(c)
+    c.remove()
+}
+document.addEventListener("DOMContentLoaded",delz);
